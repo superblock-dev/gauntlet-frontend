3a352e5528783369cbd39cfb69bb7e2a6d4ced31
schyun <schyun9212@gmail.com>
2021-09-21 15:59:21 +0900

    add nav arrow

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,15 +4,53 @@ import { config } from "react-spring";
 import styled from "@emotion/styled";
 import { Slide } from "./Slide";
 
+import leftNavigation from "../../assets/svgs/big-arrow-left.svg";
+import rightNavigation from "../../assets/svgs/big-arrow-right.svg";
+
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
 `;
 
+const NavigationButtons = styled.div`
+  position: absolute;
+  display: flex;
+  height: 40px;
+  top: 38.5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 40%;
+  margin-top: 1rem;
+  justify-content: space-between;
+  img {
+    height: 100%;
+  }
+`;
+
 class CustomCarousel extends Carousel {
     render() {
-        const { animationConfig, offsetRadius } = this.props;
+        const { animationConfig, offsetRadius, showNavigation } = this.props;
+
+        let navigationButtons = null;
+        if (showNavigation) {
+          navigationButtons = (
+            <NavigationButtons>
+              <img
+                src={leftNavigation}
+                onClick={() => this.moveSlide(-1)}
+                style={{ marginRight: "2rem" }}
+              />
+    
+              <img
+                src={rightNavigation}
+                onClick={() => this.moveSlide(1)}
+                style={{ marginLeft: "2rem" }}
+              />
+            </NavigationButtons>
+          );
+        }
+
         return (
           <React.Fragment>
             <Wrapper>
@@ -28,6 +66,7 @@ class CustomCarousel extends Carousel {
                   />
                 )
               )}
+              {navigationButtons}
             </Wrapper>
           </React.Fragment>
         );
